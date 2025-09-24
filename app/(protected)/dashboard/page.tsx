9aa8397ced1424053302e9b27e21c7a191c1a7b6
Mert Yıldız <mert@Mert-MacBook-Pro.local>
2025-09-24 20:43:43 +0300

    Created dashboard page route

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,25 +1,15 @@
 import { createClient } from '@/lib/supabase/server';
-import { DashboardClient } from './DashboardClient';
-import { getActiveBeacons, type BeaconResult } from '@/lib/services/beacon';
 
 export default async function DashboardPage() {
     const supabase = await createClient();
     const {
         data: { user },
     } = await supabase.auth.getUser();
+    //project_applicaitoın
+    const { data: project_applications } = await supabase.from('project_applications').select('*');
 
-    const { data: profile } = await supabase
-        .from('users')
-        .select('full_name, avatar_url, username')
-        .eq('id', user?.id)
-        .single();
+    const owner_data = project_applications?.filter((application) => application.applicant_id !== user?.id);
+    const applicant_data = project_applications?.filter((application) => application.applicant_id === user?.id);
 
-    let beacons: BeaconResult[] = [];
-    try {
-        beacons = await getActiveBeacons(10);
-    } catch (error) {
-        console.error('Failed to fetch beacons:', error);
-    }
-
-    return <DashboardClient beacons={beacons} user={user} profile={profile} />;
+    return <div>dashboard</div>;
 }
